be8d6a71a9f2345ea07c97bd8fa775cc8a03219a
ndafarhan <rusnandafarhan@gmail.com>
2025-05-01 23:19:22 +0700

    Refactor agent workflow by removing the SupervisorAsToolsWorkflow class and integrating SQL query execution tools

--- src/agent/workflow.py
+++ src/agent/workflow.py
@@ -5,82 +5,48 @@ Refer to the documentation for more details: https://langchain-ai.github.io/lang
 
 
 from langgraph.graph import StateGraph, END
+from langgraph.graph.message import add_messages
 from langgraph.types import Command
+from langgraph.prebuilt import create_react_agent
 from typing import Literal, Annotated, Sequence
 from pydantic import BaseModel, Field
-from langchain_core.messages import BaseMessage, HumanMessage, ToolMessage
-from langgraph.graph.message import add_messages
+from langchain_core.messages import SystemMessage, HumanMessage, ToolMessage
 from langchain_core.language_models import BaseChatModel
-from agent_state import State
+from langchain_community.utilities import SQLDatabase
+from langchain_core.tools import tool
+from agent_state import State, SQLOutput
+from loguru import logger
+from llm import LLM
+from supervisor_prompt import SYSTEM_PROMPT
+from llm_tools import (
+    get_current_datetime,
+    ExecuteQueryTool,
+    DatabaseInformationTool,
+)
 
 
-class SupervisorAsToolsWorkflow:
-    """Workflow with supervisor as tools architecture."""   
+llm = LLM.groq()
+db = SQLDatabase.from_uri("sqlite:///Chinook.db")
 
-    def __init__(self, supervisor_llm: BaseChatModel):
-            self.supervisor_llm = supervisor_llm
+@tool
+def get_data_from_database_tool(question: str):
+    """
+    Get data from the database.
+    """
+    query_agent = create_react_agent(
+        llm=llm,
+        tools=[ExecuteQueryTool(db=db), DatabaseInformationTool(db=db)],
+        response_format=SQLOutput,
+    )
+    messages = [HumanMessage(content=question)]
+    response = query_agent.invoke(messages)
+    return response['structured_response'].model_dump()
 
-    def supervisor_entry(self, state: State) -> Command[Literal["supervisor_call_model"]]:
-        """Entry point for Supervisor Agent."""
-        question = state.question.strip()
-        human_message = [HumanMessage(
-            content=question,
-        )]
-        return Command(
-            goto="supervisor_call_model",
-            upate={
-                "question": question,
-                "supervisor_messages": human_message
-            }
-        )
+supervisor_tools = [get_current_datetime, get_data_from_database_tool,]
+supervisor_agent = create_react_agent(
+    llm=llm,
+    tools=supervisor_tools,
+    prompt=SystemMessage(content=SYSTEM_PROMPT),
+)
 
-    def supervisor_call_model(self, state: State) -> Command:
-        """Call the model for Supervisor Agent."""
-        supervisor_messages = state.supervisor_messages
-        response = self.supervisor_llm.invoke(supervisor_messages)
-        if response.tool_calls:
-            goto = "supervisor_tool_call"
-        else:
-            goto = END
-        return Command(
-            goto=goto,
-            update={
-                "supervisor_messages": [response]
-            }
-        )
 
-    def sql_entry(self, state: State):
-         """Entry point for the SQL Agent."""
-         return Command(
-            goto="sql_generate_query",
-            update={
-                "sql_question": state.question.strip(),
-            }
-        )
-    
-    def sql_query_to_database(self, state: State) -> Command:
-        """Generate and Exexute the SQL Agent."""
-        sql_question = state.sql_question
-        return Command(
-            goto="sql_finalization",
-            update={
-                "sql_query": sql_question,
-                "sql_result": None,
-                "sql_error": None,
-            }
-        )
-    
-    def sql_finalization(self, state: State) -> Command:
-        """Finalization SQL Agent"""
-        sql_result = state.sql_result
-        tool_call = state.supervisor_messages[-1].tool_calls[0]
-        tool_name = tool_call["tool_name"]
-        return Command(
-            goto="supervisor_call_model",
-            update={
-                "supervisor_messages": [ToolMessage(
-                    tool_name=tool_name,
-                    content=sql_result,
-                )],
-            }
-        )
